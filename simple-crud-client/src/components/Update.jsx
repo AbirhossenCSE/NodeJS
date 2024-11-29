1ca6069beca38ee96b39112059824ccbdbd2755d
Abir Hossen <abir.cse4@gmail.com>
2024-11-29 18:34:42 +0600

    done

--- simple-crud-client/src/components/Update.jsx
+++ simple-crud-client/src/components/Update.jsx
@@ -3,13 +3,29 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
     const loadedUser = useLoaderData();
 
-    const handleUpdate = event =>{
+    const handleUpdate = event => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
         console.log(name, email);
-        
+        const updatedUser = { name, email }
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount>0) {
+                    alert('User Updated Successfully')
+                }
+            })
+
     }
     return (
         <div>
@@ -21,9 +37,9 @@ const Update = () => {
                 <br />
                 <input type="submit" value="Update" />
             </form>
-            
+
         </div>
     );
 };
-
+ 
 export default Update;
