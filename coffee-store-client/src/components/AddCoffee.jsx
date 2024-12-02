61ee9e208f30620d20f016439812cc65a778f5df
Abir Hossen <abir.cse4@gmail.com>
2024-12-02 23:23:48 +0600

    add data to server

--- coffee-store-client/src/components/AddCoffee.jsx
+++ coffee-store-client/src/components/AddCoffee.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
 
-    const handleAddCoffee = event =>{
+    const handleAddCoffee = event => {
         event.preventDefault();
 
         const form = event.target;
@@ -14,9 +15,29 @@ const AddCoffee = () => {
         const category = form.category.value;
         const details = form.details.value;
         const photo = form.photo.value;
-        const newCoffie = {name, quantity, supplier, taste, category, details, photo}
+        const newCoffie = { name, quantity, supplier, taste, category, details, photo }
         console.log(newCoffie);
-        
+
+        // Send to the server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffie)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                      })
+                }
+            })
     }
     return (
         <div className='bg-[#F4F3F0] p-24'>
