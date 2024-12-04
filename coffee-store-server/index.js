7e912fdca27fe93dc7b0a59cc8a72870b661d092
Abir Hossen <abir.cse4@gmail.com>
2024-12-05 00:16:48 +0600

    done

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -91,6 +91,29 @@ async function run() {
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     })
+    // app.patch('/users/:email', async(req, res) =>{
+    //   const email = req.params.email;
+    //   const filter = {email};
+    //   const updatedDoc = {
+    //     $set: {
+    //       lastSignInTime: req.body?.lastSignInTime
+    //     }
+    //   }
+    //   const result = await userCollection.updateOne(filter, updatedDoc);
+    //   express.send(result);
+    // })
+    app.patch('/users', async(req, res) =>{
+      const email = req.body.email;
+      const filter = {email};
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      express.send(result);
+    })
+
     app.delete('/users/:id', async (req, res) =>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
