be509c8e1e8904d645fdb8cf3dd01161a28b5ee0
Abir Hossen <abir.cse4@gmail.com>
2024-12-04 21:18:31 +0600

    display users

---
+++ coffee-store-client-p/src/components/Users.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Users = () => {
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    return (
+        <div>
+            <h2 className='text-3xl'>Users: {users.length} </h2>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created At</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>1</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.createAt}</td>
+                                <td>
+                                    <button className='btn'>E</button>
+                                    <button className='btn'>x</button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Users;
