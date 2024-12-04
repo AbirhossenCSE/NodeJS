d95ca36c0a9f4df8a436ba1ebcc7feff70f5c675
Abir Hossen <abir.cse4@gmail.com>
2024-12-04 19:00:36 +0600

    Client 2

---
+++ coffee-store-client-p/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBH5sAR3S7EDJc4nI83BXbX5JhIc6mc-tI",
+  authDomain: "simple-firebase-df58a.firebaseapp.com",
+  projectId: "simple-firebase-df58a",
+  storageBucket: "simple-firebase-df58a.firebasestorage.app",
+  messagingSenderId: "790290866361",
+  appId: "1:790290866361:web:8c15f7001fb758fe23fb68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
