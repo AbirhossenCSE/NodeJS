126cb9459ef8f20e0b3f9ad58a2a844a56507c05
Abir Hossen <abir.cse4@gmail.com>
2024-12-14 20:16:40 +0600

    tanstack

--- coffee-store-client-p/src/components/Users2.jsx
+++ coffee-store-client-p/src/components/Users2.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Users2 = () => {
+    const [users, setUsers] = useState([]);
 
     const handleUserDelete = id => {
-            Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!"
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        title: "Deleted!",
-                        text: "Your file has been deleted.",
-                        icon: "success"
-                    });
-                    // delete data
-                    fetch(`http://localhost:5000/users/${id}`, {
-                        method: 'DELETE'
-                    })
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+                // delete data
+                fetch(`http://localhost:5000/users/${id}`, {
+                    method: 'DELETE'
+                })
                     .then(res => res.json())
                     .then(data => {
                         if (data.deletedCount) {
@@ -30,21 +31,53 @@ const Users2 = () => {
                                 text: "Your file has been deleted.",
                                 icon: "success"
                             });
-    
+
                             // // update the loaded coffee state
                             // const remainingUsers = users.filter(user => user._id !== id);
                             // setUsers(remainingUsers);
                         }
-                        
+
                     })
-    
-                }
-            });
-        }
+
+            }
+        });
+    }
 
     return (
         <div>
-            
+            <h2 className='text-3xl'>Users: {users.length} </h2>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created At</th>
+                            <th>Last Signin At</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>1</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.createAt}</td>
+                                <td>{user.lastSignInTime}</td>
+                                <td>
+                                    <button className='btn'>E</button>
+                                    <button onClick={() => handleUserDelete(user._id)} className='btn'>X</button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
