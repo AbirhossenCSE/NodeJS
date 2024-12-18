9098f5521f6480fcb7757b0b245eb93b42d66c57
Abir Hossen <abir.cse4@gmail.com>
2024-12-19 01:28:38 +0600

    image animation

--- job-portal-client/src/pages/Home/Banner.jsx
+++ job-portal-client/src/pages/Home/Banner.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { motion } from "framer-motion";
+import team1 from '../../assets/Team/team-1.jpg'
+import team2 from '../../assets/Team/team-2.jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className='flex-1'>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <motion.img
+                        animate={{ y: [0, 30, 0]}}
+                        transition={{duration: 5, repeat: Infinity}}
+                        src={team1}
+                        className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-blue-400 border-l-4 border-b-4 shadow-2xl" />
+                    <motion.img
+                        animate={{ x: [100, 150, 100]}}
+                        transition={{duration: 5, repeat: Infinity}}
+                        src={team2}
+                        className="max-w-sm rounded-t-[40px] rounded-br-[40px] border-blue-400 border-l-4 border-b-4 shadow-2xl" />
                 </div>
                 <div className='flex-1'>
                     <motion.h1
-                        animate={{ x: 50 }}
+                        animate={{ x: [0, 50, 0] }}
                         transition={{ duration: 3, delay: 1, ease: "linear", repeat:Infinity }}
-                        className="text-5xl font-bold">Latest Job for You</motion.h1>
+                        className="text-5xl font-bold">Latest <motion.span animate={{color: ['#ecff33', '#333fe3', '#ff6133']}} transition={{duration: 2, repeat: Infinity}} >Job</motion.span> for You</motion.h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
