3bbc27bd9c46c97eb24be9ff0c2beb3314e99f56
Abir Hossen <abir.cse4@gmail.com>
2024-12-20 00:01:33 +0600

    application form

--- job-portal-client/src/pages/JobApply/JobApply.jsx
+++ job-portal-client/src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const JobApply = () => {
+    const {id} = useParams();
+    
+    const submitJobApplication = e => {
+        e.preventDefault();
+        const form = e.target;
+        const linkedIn = form.linkedIn.value;
+        const github = form.github.value;
+        const resume = form.resume.value;
+        console.log(linkedIn, github, resume);
+        
+
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -12,27 +25,27 @@ const JobApply = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={submitJobApplication} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">LinkedIn URL</span>
                             </label>
-                            <input type="url" placeholder="LinkedIn URL" className="input input-bordered" required />
+                            <input type="url" name='linkedIn' placeholder="LinkedIn URL" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">GitHub URL</span>
                             </label>
-                            <input type="url" placeholder="GitHub URL" className="input input-bordered" required />    
+                            <input type="url" name='github' placeholder="GitHub URL" className="input input-bordered" required />    
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Resume URL</span>
                             </label>
-                            <input type="url" placeholder="Resume URL" className="input input-bordered" required />    
+                            <input type="url" name='resume' placeholder="Resume URL" className="input input-bordered" required />    
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Apply</button>
                         </div>
                     </form>
                 </div>
