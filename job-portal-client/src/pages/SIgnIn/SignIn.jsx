0c5a0d7632e57b6c56527cd113ba247631aded1e
Abir Hossen <abir.cse4@gmail.com>
2024-12-19 22:49:57 +0600

    auth redirect

--- job-portal-client/src/pages/SIgnIn/SignIn.jsx
+++ job-portal-client/src/pages/SIgnIn/SignIn.jsx
@@ -3,9 +3,14 @@ import signinLottile from '../../assets/Lottile/signin.json'
 import Lottie from 'lottie-react';
 import AuthContext from '../../context/AuthContext/AuthContext';
 import SocialLogin from '../shared/SocialLogin';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
     const { signInUser } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state || '/';
+
     const handleSignIn = e => {
         e.preventDefault();
         const form = e.target;
@@ -17,6 +22,7 @@ const SignIn = () => {
         signInUser(email, password)
             .then(result => {
                 console.log("signin", result.user);
+                navigate(from);
             })
             .catch(error => {
                 console.log(error);
