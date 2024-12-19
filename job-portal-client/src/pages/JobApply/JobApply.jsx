1e1dcaf1945f967de3c6319300599da45ba1847c
Abir Hossen <abir.cse4@gmail.com>
2024-12-19 23:50:40 +0600

    url

--- job-portal-client/src/pages/JobApply/JobApply.jsx
+++ job-portal-client/src/pages/JobApply/JobApply.jsx
@@ -21,9 +21,9 @@ const JobApply = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">GitHub URL</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="url" placeholder="GitHub URL" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
