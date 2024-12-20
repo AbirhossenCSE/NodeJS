bab4da37d31d0ac15cb828936f71dcfbdba15294
Abir Hossen <abir.cse4@gmail.com>
2024-12-21 01:24:13 +0600

    Update Status

--- job-portal-client/src/pages/ViewApplications/ViewApplications.jsx
+++ job-portal-client/src/pages/ViewApplications/ViewApplications.jsx
@@ -3,6 +3,25 @@ import { useLoaderData } from 'react-router-dom';
 
 const ViewApplications = () => {
     const applications = useLoaderData();
+
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id);
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        
+    }
     return (
         <div>
             <h2 className='text-2xl'>Application for this Job: {applications.length} </h2>
@@ -14,20 +33,31 @@ const ViewApplications = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Status</th>
+                            <th> Update Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             applications.map((app, i) => <tr key={app._id}>
-                                <th>{i +1}</th>
+                                <th>{i + 1}</th>
                                 <td>{app.applicant_email}</td>
                                 <td>Quality Control Specialist</td>
-                                <td>Blue</td>
+                                <td>
+                                    <select
+                                        onChange={(e) => handleStatusUpdate(e, app._id)}
+                                        defaultValue={app.status || 'Changed status'}
+                                        className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled >Changed status</option>
+                                        <option>Under Revew</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
                             </tr>)
                         }
-                        
+
                     </tbody>
                 </table>
             </div>
