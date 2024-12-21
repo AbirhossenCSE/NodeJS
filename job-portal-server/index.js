72bb5fea285d2613103dc0dd90960e1414f7b5cb
Abir Hossen <abir.cse4@gmail.com>
2024-12-21 17:42:47 +0600

    Httponly Cookie

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
-
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wpavw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -144,11 +148,17 @@ async function run() {
 
 
 
-         // Auth Related Api
-         app.post('/jwt', async (req, res) => {
+        // Auth Related Api
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, 'secret', { expiresIn: '1h' });
-            res.send(token);
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    // set true on production time
+                    secure: false,
+                })
+                .send({ success: true });
         })
 
 
@@ -160,9 +170,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.use(cors());
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Job is falling from the sky')
